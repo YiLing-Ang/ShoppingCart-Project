cccf9dc2040ea16e95a5465a73dcf5e92dce01ba
e0941863 <103227996+e0941863@users.noreply.github.com>
2022-04-08 15:37:49 +0800

    3rd commit
    HomeController
    the gallery page.
    added search and validate session method

--- team5_SC/Controllers/HomeController.cs
+++ team5_SC/Controllers/HomeController.cs
@@ -1,59 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using team5_SC.Models;
 
 namespace team5_SC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private DBContext dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(DBContext dbContext)
         {
-            _logger = logger;
+            this.dbContext = dbContext;
         }
 
-        public IActionResult Index()
+        public IActionResult Search(string searchStr)
         {
+            Session session = ValidateSession();
+
+            if (searchStr == null)
+            {
+                searchStr = "";
+            }
+
+            List<Product> products = dbContext.Products.Where(x =>
+                x.Name.Contains(searchStr) ||
+                x.Description.Contains(searchStr) ||
+                x.Price.Contains(searchStr)
+            ).ToList();
+
+            ViewData["searchStr"] = searchStr;
+            ViewData["products"] = products;
             return View();
         }
 
-        public IActionResult Privacy()
+        private Session ValidateSession()
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        
-
-        public IActionResult Logout()
-        {
-            string sessionId = Request.Cookies["SessionId"];
-            if (sessionId == null)
+            // check if there is a SessionId cookie
+            if (Request.Cookies["SessionId"] == null)
             {
-                return RedirectToAction("Login");
+                return null;
             }
 
-            int res = SessionData.DeleteSesion(sessionId);
-            if (res != 1)
-            {
-                return StatusCode(500);
-            }
+            // convert into a Guid type (from a string type)
+            Guid sessionId = Guid.Parse(Request.Cookies["SessionId"]);
+            Session session = dbContext.Sessions.FirstOrDefault(x =>
+                x.Id == sessionId
+            );
 
-            Response.Cookies.Delete("SessionId");
-
-            return RedirectToAction("Login", "Home");
+            return session;
         }
+
     }
-}
 }
--- team5_SC/Controllers/LoginController.cs
+++ team5_SC/Controllers/LoginController.cs
@@ -87,5 +87,11 @@ namespace team5_SC.Controllers
             // no Session ID; show Login page
             return View();
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
