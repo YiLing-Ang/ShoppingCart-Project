cd373410b31d952c96296344aed750500a2a918b
HsuMonKo <monko944@gmail.com>
2022-04-12 00:01:30 +0800

    Update MyPurchase.cs

--- team5_SC/Models/MyPurchase.cs
+++ team5_SC/Models/MyPurchase.cs
@@ -15,7 +15,5 @@ namespace team5_SC.Models
         public DateTime PurchaseDate { get; set; }
         public virtual Guid UserId { get; set; }
         public virtual ICollection<Product> Products { get; set; }
-
-        public virtual Guid ActivationCodeId { get; set; }
     }
 }
